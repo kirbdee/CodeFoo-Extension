c5d636250582acdbd9154e5a6a8236c3b81aa070
Kirby Domingo <kdomingo@berngp-mbp.hq.ign.com>
2012-08-07 17:49:25 -0700

    added psuedo notifications and base html and scripts

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.executeScript(
+	null, {code:"document.body.style.background='red !important'"});
+	});
+
+	chrome.browserAction.setBadgeBackgroundColor({color:[0, 200, 0, 100]});
+
+	var i = 0;
+	window.setInterval(function() {
+  		chrome.browserAction.setBadgeText({text:String(i)});
+  		i=i+100;
+	}, 1000);
