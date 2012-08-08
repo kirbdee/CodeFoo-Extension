fc7401b9d92c6d5a2961977124f9171e4f7a6335
Kirby Domingo <kdomingo@berngp-mbp.hq.ign.com>
2012-08-07 20:29:42 -0700

    Started json consumtion

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,46 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript(
-	null, {code:"document.body.style.background='red !important'"});
+var IGNENDPOINTS = {
+	articles:"http://apis.ign.com/article/v3/articles/search?format=js&callback=?&variable=var&q=",
+	images:"http://apis.ign.com/image/v3/images/search?format=js&callback=?&variable=var&q=",
+	video:"http://video-api.ign.com/v3/videos/search?format=js&callback=?&variable=var&q="
+};
+
+var sampleMeta = '{"matchRule":"matchAll","count":25,"startIndex":0,"networks":"ign","states":"published","sortBy":"metadata.publishDate","sortOrder":"desc","rules":[{"field":"metadata.articleType","condition":"is","value":"article"},{"field":"categoryLocales","condition":"contains","value":"us"},{"field":"tags","condition":"containsOne","value":"vita,ps3"}]}';
+var encodedMeta = encodeURI(sampleMeta);
+var articlesUrl = IGNENDPOINTS.articles+encodedMeta;
+
+$.getJSON(articlesUrl,
+	function(data){
+		$('#wrapper').append(data.count);
 	});
 
-	chrome.browserAction.setBadgeBackgroundColor({color:[0, 200, 0, 100]});
+$(document).ready(
+	function(){
+		chrome.browserAction.setBadgeText({text:String('')});
 
-	var i = 0;
-	window.setInterval(function() {
-  		chrome.browserAction.setBadgeText({text:String(i)});
-  		i=i+100;
-	}, 1000);
+		// GET OPTIONS
+		// BUILD LIST
+		buildList();
+});
+
+function buildList(){
+
+	// GET ARTICLES
+	$.getJSON(articlesUrl,
+		function(response){
+			data = response.data;
+			for (i = 0; i < data.length; i++)
+			{
+				full = split(data.metadata.publishDate,"T");
+				headline = data.metadata.headline;
+				date = split(date,"-");
+				year = date[0];
+				month = date[1];
+				day = date[2];
+				$('#listView ul').append('<li><a target="_blank" href="http://www.ign.com/articles/'+'">'+headline+'</a></li>');
+				console.log(data[i]);
+			}
+		});
+		// GET IMAGES
+	// GET VIDEOS
+	
+}
