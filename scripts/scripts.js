254623b61d6477dd8f36a6d976e3ba0276d77c40
Kirby Domingo <kdomingo@berngp-mbp.hq.ign.com>
2012-08-07 21:20:41 -0700

    add video json consumption

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,17 +8,28 @@ var sampleMeta = '{"matchRule":"matchAll","count":10,"startIndex":0,"networks":"
 var encodedMeta = encodeURI(sampleMeta);
 var articlesUrl = IGNENDPOINTS.articles+encodedMeta;
 
+var videoMeta = '{"matchRule":"matchAny","count":25,"startIndex":0,"networks":"ign","states":"published","sortBy":"metadata.publishDate","sortOrder":"desc","rules":[{"field":"tags","condition":"containsOne","value":"xbox-360"},{"field":"tags","condition":"containsOne","value":"ps3"}]}';
+var encodedVidMeta = encodeURI(videoMeta);
+var videosUrl = IGNENDPOINTS.video+encodedVidMeta;
+
 $(document).ready(
 	function(){
-		chrome.browserAction.setBadgeText({text:String('')});
+		//chrome.browserAction.setBadgeText({text:String('')});
 
 		// GET OPTIONS
 		// BUILD LIST
+		$(document).blur();
 		buildList();
 });
 
 function buildList(){
 
+	// GET VIDEOS
+	var videoArray;
+	$.getJSON(videosUrl,
+		function(response){
+			videoArray = response.data;
+		});
 	// GET ARTICLES
 	$.getJSON(articlesUrl,
 		function(response){
@@ -36,9 +47,8 @@ function buildList(){
 				articleUrl = 'http://www.ign.com/articles/'+year+'/'+month+'/'+day+'/'+slug;
 				$('#listView ul').append('<li><a target="_blank" href="'+articleUrl+'">'+headline+'</a></li>');
 				console.log(data[i]);
+				// GET IMAGES
 			}
 		});
-		// GET IMAGES
-	// GET VIDEOS
 	
 }
